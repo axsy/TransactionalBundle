fd53e7865326dac0f4a3fd1a924261890a14344a
Aleksey Orlov <i.trancer@gmail.com>
2013-04-28 18:41:00 +0300

    Initial commit

---
+++ AxsyTransactionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Axsy\TransactionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AxsyTransactionBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+AxsyTransactionBundle
+=====================
+
+This bundle provides simple transactional wrapper for the controller and services.
