db20cb3bf169f95ace74fddc8b84eb0afb842d66
Aleksey Orlov <i.trancer@gmail.com>
2013-05-04 01:48:11 +0300

    Functional testing boostrap fix

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,7 +4,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 // Let's use closure to keep global context clean
 call_user_func(function() {
-    if (!is_file($autoloader = __DIR__ . '/../../../../vendor/autoload.php')) {
+    if (!is_file($autoloader = __DIR__ . '/../../../../../autoload.php')) {
         throw new \Exception('Can\'t find autoload configuration. Did you install vendors?');
     }
     require_once $autoloader;
